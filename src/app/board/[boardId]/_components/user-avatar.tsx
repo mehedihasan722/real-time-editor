4f1da13679a42e854b6b6296da4f1cdcac2adcdd
mehedihasan722 <mjimehedi99@google.com>
2024-09-08 17:04:10 +0600

    Room Participants Complete

---
+++ src/app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Hint from "@/components/hint";
+
+interface UserAvatarProps {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  borderColor?: string;
+}
+
+const UserAvatar = ({ src, name, fallback, borderColor }: UserAvatarProps) => {
+  return (
+    <Hint label={name || "Teammate"} side="bottom" sideOffset={18}>
+      <Avatar className="h-8 w-8 border-2" style={{ borderColor }}>
+        <AvatarImage src={src} />
+        <AvatarFallback className="text-xs font-semibold">
+          {fallback}
+        </AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
+};
+
+export default UserAvatar;
